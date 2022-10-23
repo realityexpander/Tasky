72091ba4af951db3523d4a7f648c31a41c8c45b7
Chris Athanas <realityexpander@gmail.com>
2022-10-23 18:34:52 -0500

    Improve compose layout

--- app/src/main/java/com/realityexpander/tasky/presentation/login_screen/LoginScreen.kt
+++ app/src/main/java/com/realityexpander/tasky/presentation/login_screen/LoginScreen.kt
@@ -1,8 +1,6 @@
 package com.realityexpander.tasky.presentation.login_screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -10,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -25,7 +24,11 @@ fun LoginScreen(
     val loginState by viewModel.loginStateFlow.collectAsState()
     val scope = rememberCoroutineScope()
 
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
         Text(text = "Login:")
         Spacer(modifier = Modifier.height(8.dp))
 
@@ -38,7 +41,8 @@ fun LoginScreen(
             },
             isError = loginState.isInvalidEmail,
             label = { Text(text = "Email") },
-            placeholder = { Text(text = "Enter your Email") }
+            placeholder = { Text(text = "Enter your Email") },
+            modifier = Modifier.fillMaxWidth()
         )
         if(loginState.isInvalidEmail) {
             Text(text = "Invalid email", color = Color.Red)
@@ -54,28 +58,43 @@ fun LoginScreen(
             },
             isError = loginState.isInvalidPassword,
             label = { Text(text = "Password") },
-            placeholder = { Text(text = "Enter your Password") }
+            placeholder = { Text(text = "Enter your Password") },
+            modifier = Modifier.fillMaxWidth()
         )
         if(loginState.isInvalidPassword) {
             Text(text = "Invalid password", color = Color.Red)
             Spacer(modifier = Modifier.height(8.dp))
         }
 
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {
-            scope.launch {
-                viewModel.onEvent(LoginEvent.ValidateEmail(loginState.email))
-                viewModel.onEvent(LoginEvent.ValidatePassword(loginState.password))
-                viewModel.onEvent(LoginEvent.Login(loginState.email, loginState.password))
-            }
-        }) {
+                scope.launch {
+                    viewModel.onEvent(LoginEvent.ValidateEmail(loginState.email))
+                    viewModel.onEvent(LoginEvent.ValidatePassword(loginState.password))
+                    viewModel.onEvent(LoginEvent.Login(loginState.email, loginState.password))
+                }
+            },
+            enabled = !loginState.isLoading,
+            modifier = Modifier
+                .align(alignment = Alignment.End)
+        ) {
             Text(text = "Login")
         }
+        Spacer(modifier = Modifier.height(32.dp))
 
-        Text(text = "Not a member? Sign up")
-        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Not a member? Sign up",
+            color = Color.Cyan,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Forgot password?")
-        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Forgot password?",
+            color = Color.Cyan,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
 
         if(loginState.isError) {
             Text(text = "Error: ${loginState.errorMessage}")
