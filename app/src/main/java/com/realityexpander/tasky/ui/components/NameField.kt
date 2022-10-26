1c9bd8a25084fb2f3d2e0f31d5cf0d0414876fa3
Chris Athanas <realityexpander@gmail.com>
2022-10-26 00:31:17 -0500

    Add NameField Component

---
+++ app/src/main/java/com/realityexpander/tasky/ui/components/NameField.kt
@@ -0,0 +1,77 @@
+package com.realityexpander.tasky.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.People
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import com.realityexpander.tasky.R
+import com.realityexpander.tasky.common.UiText
+
+@Composable
+fun NameField(
+    value: String,
+    label: String? = UiText.Res(R.string.nameField_label).get(), // if this is null, label is not shown
+    labelComponent: @Composable (() -> Unit)? =
+        { Text(text = label ?: UiText.Res(R.string.nameField_label).get()) },
+    placeholder: String = UiText.Res(R.string.nameField_placeholder).get(),
+    isError: Boolean,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions =
+        KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next),
+    keyboardActions: KeyboardActions? = null,
+) {
+    val focusManager = LocalFocusManager.current
+    val keyboardActionsLocal: KeyboardActions = keyboardActions ?: KeyboardActions(
+        onNext = {
+            focusManager.moveFocus(FocusDirection.Down)
+        }
+    )
+
+    OutlinedTextField(
+        value = value,
+        singleLine = true,
+        onValueChange = onValueChange,
+        isError = isError,
+        label = if(label != null) labelComponent else null,
+        placeholder = { Text(text = placeholder) },
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActionsLocal,
+        modifier = Modifier.fillMaxWidth(),
+        leadingIcon = {
+            Icon(imageVector = Icons.Filled.People,
+                UiText.Res(R.string.nameField_description_name).get())
+        },
+        trailingIcon = {
+            if(value.isNotBlank()) {
+                val isNameValid = !isError
+
+                val image =
+                    if (isNameValid)
+                        Icons.Filled.Check
+                    else
+                        Icons.Filled.Error
+
+                // localized description for accessibility services
+                val description =
+                    if (isNameValid)
+                        UiText.Res(R.string.nameField_description_isValid).get()
+                    else
+                        UiText.Res(R.string.nameField_description_isInvalid).get()
+
+                Icon(imageVector = image, description)
+            }
+        }
+    )
+}
