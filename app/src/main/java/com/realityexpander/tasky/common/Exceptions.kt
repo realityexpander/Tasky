d04fe98ccde22d74613b15455f68919f6ae9b6d5
Chris Athanas <realityexpander@gmail.com>
2022-10-26 00:56:28 -0500

    Add username exceptions, improve messages

--- app/src/main/java/com/realityexpander/tasky/common/Exceptions.kt
+++ app/src/main/java/com/realityexpander/tasky/common/Exceptions.kt
@@ -1,20 +1,22 @@
 package com.realityexpander.tasky.common
 
 object Exceptions {
+    const val USERNAME_NOT_VALID = "Username is not valid"
     const val EMAIL_NOT_VALID = "Email is not valid"
     const val PASSWORD_NOT_VALID = "Password is not valid"
-    const val PASSWORDS_NOT_MATCH = "Passwords do not match"
+    const val PASSWORDS_DO_NOT_MATCH = "Passwords do not match"
     const val EMAIL_ALREADY_EXISTS = "Email already exists"
-    const val EMAIL_NOT_EXISTS = "Email does not exist"
+    const val EMAIL_DOES_NOT_EXIST = "Email does not exist"
     const val WRONG_PASSWORD = "Wrong password"
     const val UNKNOWN_ERROR = "Unknown error"
     const val LOGIN_ERROR = "Login error"
 
+    class InvalidUsernameException : Exception(USERNAME_NOT_VALID)
     class InvalidEmailException: Exception(EMAIL_NOT_VALID)
     class InvalidPasswordException: Exception(PASSWORD_NOT_VALID)
-    class PasswordsNotMatchException: Exception(PASSWORDS_NOT_MATCH)
+    class PasswordsNotMatchException: Exception(PASSWORDS_DO_NOT_MATCH)
     class EmailAlreadyExistsException: Exception(EMAIL_ALREADY_EXISTS)
-    class EmailNotExistsException: Exception(EMAIL_NOT_EXISTS)
+    class EmailNotExistsException: Exception(EMAIL_DOES_NOT_EXIST)
     class WrongPasswordException: Exception(WRONG_PASSWORD)
     class UnknownErrorException: Exception(UNKNOWN_ERROR)
     class LoginException(message: String? = null): Exception(message ?: LOGIN_ERROR)
