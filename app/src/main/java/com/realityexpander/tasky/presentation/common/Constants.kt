142fc34776853ea3b2e083d8d6a9f9942a42a3db
Chris Athanas <realityexpander@gmail.com>
2022-10-26 00:56:58 -0500

    Add username constants for savedStateHandle

--- app/src/main/java/com/realityexpander/tasky/presentation/common/Constants.kt
+++ app/src/main/java/com/realityexpander/tasky/presentation/common/Constants.kt
@@ -3,6 +3,7 @@ package com.realityexpander.tasky.presentation.common
 object UIConstants {
 
     // Login / Register screens
+    const val SAVED_STATE_username = "username"
     const val SAVED_STATE_email = "email"
     const val SAVED_STATE_password = "password"
     const val SAVED_STATE_confirmPassword = "confirmPassword"
