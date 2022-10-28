54f0dcb35019f16dfe14a460450ebe89a3d8a124
Chris Athanas <realityexpander@gmail.com>
2022-10-28 17:36:19 -0500

    Improve logic for clarity in fake auth api

--- app/src/main/java/com/realityexpander/tasky/data/repository/remote/AuthApiFakeImpl.kt
+++ app/src/main/java/com/realityexpander/tasky/data/repository/remote/AuthApiFakeImpl.kt
@@ -4,6 +4,10 @@ import com.realityexpander.tasky.common.*
 import com.realityexpander.tasky.data.repository.AuthInfo
 import kotlinx.coroutines.delay
 
+// Fake implementation of the API for testing purposes
+// Has a delay of 1 second to simulate network latency.
+// Uses fake tokens.
+
 class AuthApiFakeImpl: IAuthApi {
     private var authToken: AuthInfo? = null
     private val users =
@@ -15,39 +19,47 @@ class AuthApiFakeImpl: IAuthApi {
     }
 
     override suspend fun login(email: Email, password: Password): AuthInfo {
-        delay(1000)
+        if (email.isBlank() || password.isBlank()) {
+            throw Exceptions.LoginException("Invalid email or password")
+        }
+
+        delay(1000) // simulate network call
 
         if (users[email] == null) {
             throw Exceptions.LoginException("Unknown email")
         }
 
-        return if(users[email]?.second == password) {
-            AuthInfo(
-                AuthToken("token for $email"),
-                UserId("id for $email"),
-                Username(users[email]?.first ?: "Unknown")
-            )
-        } else {
+        if(users[email]?.second != password) {
             throw Exceptions.WrongPasswordException()
         }
+
+        return AuthInfo(
+            AuthToken("token for $email"),
+            UserId("id for $email"),
+            Username(users[email]?.first ?: "Unknown")
+        )
     }
 
     override suspend fun register(
         username: Username,
         email: Email,
         password: Password
-    ): AuthInfo {
-        delay(1000)
+    ) {
+        if (email.isBlank() || password.isBlank() || username.isBlank()) {
+            throw Exceptions.RegisterException("Invalid username, email or password")
+        }
 
-        return if(users[email] == null) {
-            users[email] = username to password
-            AuthInfo(
-                AuthToken("token for $email"),
-                UserId("id for $email"),
-                Username(username)
-            )
-        } else {
+        delay(1000) // simulate network call
+
+        if (users[email] != null) {
             throw Exceptions.EmailAlreadyExistsException()
         }
+
+        users[email] = username to password
+        AuthInfo(
+            AuthToken("token for $email"),
+            UserId("id for $email"),
+            Username(username)
+        )
     }
 }
