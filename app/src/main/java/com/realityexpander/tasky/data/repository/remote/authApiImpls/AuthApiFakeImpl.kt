ee6af84e99c013dcd4d1fa174172eb2f708fa709
Chris Athanas <realityexpander@gmail.com>
2022-10-30 01:12:37 -0600

    Add in-file tests for AuthApiFakeImpl, simulate authenticate call

--- app/src/main/java/com/realityexpander/tasky/data/repository/remote/authApiImpls/AuthApiFakeImpl.kt
+++ app/src/main/java/com/realityexpander/tasky/data/repository/remote/authApiImpls/AuthApiFakeImpl.kt
@@ -11,33 +11,16 @@ import javax.inject.Inject
 // Uses fake tokens.
 
 data class AuthInfoFakeEntity(
-    val authToken: AuthToken? = null,
+    var authToken: AuthToken? = null,
     val userId: UserId? = null,
     val username: Username? = null,
     val password: Password? = null
 )
 
-class AuthApiFakeImpl @Inject constructor(): IAuthApi {
-    private val users =
-        mutableMapOf<Email, AuthInfoFakeEntity>()
 
-    init {
-        // Setup fake users
-        users["chris@demo.com"] =
-            AuthInfoFakeEntity(
-                username = "Chris Athanas",
-                authToken = getAuthToken("chris@demo.com"),
-                userId = getUserId("chris@demo.com") ,
-                password = "Password1",
-            )
-        users["a@aa.com"] =
-            AuthInfoFakeEntity(
-                username = "Bilbo Baggins",
-                authToken = getAuthToken("a@aa.com"),
-                userId = getUserId("a@aa.com"),
-                password = "Password1",
-            )
-    }
+class AuthApiFakeImpl @Inject constructor(): IAuthApi {
+
+    /////////////////// FAKE API IMPLEMENTATION ///////////////////////
 
     override suspend fun login(
         email: Email,
@@ -59,8 +42,8 @@ class AuthApiFakeImpl @Inject constructor(): IAuthApi {
         }
 
         return AuthInfoDTO(
-            authToken(getAuthToken(email)),
-            userId(getUserId(email)),
+            authToken(generateAuthToken(email)),
+            userId(generateUserId(email)),
             username(users[email]?.username ?: "Username Missing")
         )
     }
@@ -83,26 +66,124 @@ class AuthApiFakeImpl @Inject constructor(): IAuthApi {
 
         users[email] = AuthInfoFakeEntity(
             username = username,
-            authToken = getAuthToken(email),
-            userId = getUserId(email),
+            authToken = generateAuthToken(email),
+            userId = generateUserId(email),
             password = password,
         )
         AuthInfoDTO(
-            authToken(getAuthToken(email)),
-            userId(getUserId(email)),
+            authToken(generateAuthToken(email)),
+            userId(generateUserId(email)),
             username(users[email]?.username ?: "Username Missing")
         )
     }
 
-    private fun getAuthToken(email: Email): AuthToken {
+    override suspend fun authenticate(authToken: AuthToken?): Boolean {
+        // simulate network call
+        delay(500)
+
+        authToken?.let {
+            // Check the passed-in authToken against the fake tokens
+            if (users.map { it.value.authToken }.contains(authToken)) {
+                return true
+            }
+        } ?: run {
+            // Check the logged-in user token if no token is provided
+            if(IAuthApi.authToken != null
+                && (IAuthApi.authToken?.startsWith("token") == true)
+            ) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    /////////////// Server simulation functions //////////////////////
+
+    private val users =
+        mutableMapOf<Email, AuthInfoFakeEntity>()
+
+    init {
+        // Setup fake users
+        users["chris@demo.com"] =
+            AuthInfoFakeEntity(
+                username = "Chris Athanas",
+                authToken = generateAuthToken("chris@demo.com"),
+                userId = generateUserId("chris@demo.com") ,
+                password = "Password1",
+            )
+        users["a@aa.com"] =
+            AuthInfoFakeEntity(
+                username = "Bilbo Baggins",
+                authToken = generateAuthToken("a@aa.com"),
+                userId = generateUserId("a@aa.com"),
+                password = "Password1",
+            )
+    }
+
+    private fun generateAuthToken(email: Email): AuthToken {
         return authToken("token for $email") as AuthToken
     }
 
-    private fun getUserId(email: Email): UserId {
+    private fun generateUserId(email: Email): UserId {
         return userId("id for $email") as UserId
     }
 
-    override suspend fun authenticate(authToken: AuthToken?): Boolean {
-        return authToken != null
+    suspend fun expireToken(email: Email) {
+        users[email]?.authToken = null
     }
+}
+
+suspend fun main() {
+
+    fun assert(condition: Boolean): String {
+        return if (condition) {
+            "True"
+        } else {
+            "False"
+        }
+    }
+
+    val authApiFakeImpl = AuthApiFakeImpl()
+    var user = authApiFakeImpl.login("chris@demo.com", "Password1")
+    IAuthApi.setAuthToken(user.authToken)
+
+    println("user=$user")
+
+    println("AuthApiFakeImpl.authenticate() Logged in user")
+    println("  user.authToken is valid=" + assert(authApiFakeImpl.authenticate(user.authToken)))
+    println("  logged-in user authToken is valid=" + assert(authApiFakeImpl.authenticate()))
+
+    println()
+
+    IAuthApi.setAuthToken(null)
+    println("AuthApiFakeImpl.authenticate() Logged out user has no authToken")
+    println("   IAuthApi.authToken=null, token is NOT valid locally=" + assert(!authApiFakeImpl.authenticate()))
+    println("   user.authToken still valid on server=" + assert(authApiFakeImpl.authenticate(user.authToken)))
+
+    println()
+
+    user = authApiFakeImpl.login("chris@demo.com", "Password1")
+    authApiFakeImpl.expireToken("chris@demo.com") // server expires the token
+    println("AuthApiFakeImpl.authenticate() Expired token (server side) when user is still logged in")
+    println("   logged-in user authToken is valid & not null=" + assert(user.authToken != null))
+    println("   user.authToken is INVALID on server=" + assert(!authApiFakeImpl.authenticate(user.authToken)))
+    println("   logged-in user is NOT able to authenticate their token=" + assert(!authApiFakeImpl.authenticate()))
+
+    println()
+
+    println("AuthApiFakeImpl.authenticate() Invalid token")
+    println("   invalid authToken is INVALID=" + assert(!authApiFakeImpl.authenticate(authToken("invalid token"))))
+
+    println()
+
+    println("AuthApiFakeImpl.authenticate() Null token")
+    println("   null authToken is INVALID=" + assert(!authApiFakeImpl.authenticate(null)))
+
+    println()
+
+    println("AuthApiFakeImpl.authenticate() Blank token")
+    println("   blank authToken is INVALID=" + assert(!authApiFakeImpl.authenticate(authToken(""))))
+
+    println()
 }
