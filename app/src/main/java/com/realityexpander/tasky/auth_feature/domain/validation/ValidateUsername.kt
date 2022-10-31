c7c8ce8c6e5f5fae1995cd160225bbb5deb4bcea
Chris Athanas <realityexpander@gmail.com>
2022-10-30 22:57:01 -0600

    improve username validation by using isBlank()

--- app/src/main/java/com/realityexpander/tasky/auth_feature/domain/validation/ValidateUsername.kt
+++ app/src/main/java/com/realityexpander/tasky/auth_feature/domain/validation/ValidateUsername.kt
@@ -2,7 +2,7 @@ package com.realityexpander.tasky.auth_feature.domain.validation
 
 class ValidateUsername() {
     fun validate(username: String): Boolean {
-        if(username.isEmpty()) return false
+        if(username.isBlank()) return false
 
         return username.length in 2..50             // 2-50 chars
     }
