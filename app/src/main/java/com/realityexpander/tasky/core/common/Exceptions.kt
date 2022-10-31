fa961c0c1840976bb6bf9a486469323985e193d3
Chris Athanas <realityexpander@gmail.com>
2022-10-30 19:11:14 -0600

    Improve exceptions

--- app/src/main/java/com/realityexpander/tasky/core/common/Exceptions.kt
+++ app/src/main/java/com/realityexpander/tasky/core/common/Exceptions.kt
@@ -5,9 +5,7 @@ object Exceptions {
     const val USERNAME_NOT_VALID = "Username is not valid"
     const val EMAIL_NOT_VALID = "Email is not valid"
     const val PASSWORD_NOT_VALID = "Password is not valid"
-    const val PASSWORDS_DO_NOT_MATCH = "Passwords do not match"
     const val EMAIL_ALREADY_EXISTS = "Email already exists"
-    const val EMAIL_DOES_NOT_EXIST = "Email does not exist"
     const val WRONG_PASSWORD = "Wrong password"
     const val UNKNOWN_ERROR = "Unknown error"
     const val LOGIN_ERROR = "Login error"
@@ -17,9 +15,7 @@ object Exceptions {
     class InvalidUsernameException : Exception(USERNAME_NOT_VALID)
     class InvalidEmailException: Exception(EMAIL_NOT_VALID)
     class InvalidPasswordException: Exception(PASSWORD_NOT_VALID)
-    class PasswordsNotMatchException: Exception(PASSWORDS_DO_NOT_MATCH)
     class EmailAlreadyExistsException: Exception(EMAIL_ALREADY_EXISTS)
-    class EmailNotExistsException: Exception(EMAIL_DOES_NOT_EXIST)
     class WrongPasswordException: Exception(WRONG_PASSWORD)
 
     class LoginException(message: String? = null): Exception(message ?: LOGIN_ERROR)
@@ -27,5 +23,5 @@ object Exceptions {
     class RegisterException(message: String? = null): Exception(message ?: REGISTER_ERROR)
 
     class NetworkException(message: String? = null): Exception(message ?: NETWORK_ERROR)
-    class UnknownErrorException(message: String? = null): Exception(message)
+    class UnknownErrorException(message: String? = null): Exception(message ?: UNKNOWN_ERROR)
 }
