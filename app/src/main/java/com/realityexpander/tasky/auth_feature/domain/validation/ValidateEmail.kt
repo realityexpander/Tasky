8c9176983603b5d2480c45a4028d62719d1acfe1
Chris Athanas <realityexpander@gmail.com>
2022-10-30 18:29:29 -0600

    Create simplified ValidateEmail usecase

---
+++ app/src/main/java/com/realityexpander/tasky/auth_feature/domain/validation/ValidateEmail.kt
@@ -0,0 +1,11 @@
+package com.realityexpander.tasky.auth_feature.domain.validation
+
+class ValidateEmail() {
+    fun validate(email: String): Boolean {
+        if(email.isNotBlank()) return false
+
+        // use regex to validate email
+        val regex = Regex("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", RegexOption.IGNORE_CASE)
+        return regex.matches(email)
+    }
+}
