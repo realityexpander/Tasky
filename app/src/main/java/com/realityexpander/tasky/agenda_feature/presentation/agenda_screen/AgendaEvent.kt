b2d400613c2016d896cecda4e5828dc5ca4a39d8
Chris Athanas <realityexpander@gmail.com>
2022-11-02 02:21:22 -0600

    add events to AgendaEvent.kt

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/presentation/agenda_screen/AgendaEvent.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/presentation/agenda_screen/AgendaEvent.kt
@@ -3,33 +3,27 @@ package com.realityexpander.tasky.agenda_feature.presentation.agenda_screen
 import com.realityexpander.tasky.core.presentation.common.util.UiText
 
 sealed interface AgendaEvent {
+    data class SetIsLoaded(val isLoaded: Boolean) : AgendaEvent
     data class ShowProgressIndicator(val isShowing: Boolean) : AgendaEvent
 
-    data class UpdateUsername(val username: String) : AgendaEvent
-    data class UpdateEmail(val email: String) : AgendaEvent
-    data class UpdatePassword(val password: String) : AgendaEvent
-    data class UpdateConfirmPassword(val confirmPassword: String) : AgendaEvent
-
     object ValidateUsername : AgendaEvent
-
     data class IsPasswordsMatch(val isMatch: Boolean) : AgendaEvent
 
     object ShowInvalidUsernameMessage : AgendaEvent
 
-    data class Register(
+    data class UnknownError(val message: UiText) : AgendaEvent
+
+    data class CreateEvent(
             val username: String,
             val email: String,
             val password: String,
             val confirmPassword: String
         ) : AgendaEvent
-    data class RegisterSuccess(val message: UiText) : AgendaEvent
-    data class RegisterError(val message: UiText) : AgendaEvent
-
-    data class UnknownError(val message: UiText) : AgendaEvent
-
+    data class CreateEventSuccess(val message: UiText) : AgendaEvent
+    data class CreateEventError(val message: UiText) : AgendaEvent
 
 
     object ToggleLogoutDropdown : AgendaEvent
     object Logout : AgendaEvent
-    data class SetIsLoaded(val isLoaded: Boolean) : AgendaEvent
+
 }
