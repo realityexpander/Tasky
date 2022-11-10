8fb8f425728c5113aa813d6371401f7bcd19ba1b
Chris Athanas <realityexpander@gmail.com>
2022-11-10 17:29:06 -0600

    Add emptyId for empty lists that are required by Api, add type wrappers

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/util/AgendaUtil.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/util/AgendaUtil.kt
@@ -7,5 +7,14 @@ typealias ReminderId = UuidStr
 typealias TaskId = UuidStr
 typealias AttendeeId = UuidStr
 typealias PhotoId = UuidStr
+const val emptyId = ""
+typealias UrlStr = String
 
 typealias TimeZoneStr = String  // ex: "America/Los_Angeles"
+
+fun attendeeId(id: String): AttendeeId = id
+fun eventId(id: String): EventId = id
+fun reminderId(id: String): ReminderId = id
+fun taskId(id: String): TaskId = id
+fun photoId(id: String): PhotoId = id
+
