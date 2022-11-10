0bac589e033c96d8644255833dd2d007fff972c0
Chris Athanas <realityexpander@gmail.com>
2022-11-10 17:27:09 -0600

    Add flow for database query

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/agendaRepository/agendaRepositoryImpls/AgendaRepostoryImpl.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/agendaRepository/agendaRepositoryImpls/AgendaRepostoryImpl.kt
@@ -3,23 +3,31 @@ package com.realityexpander.tasky.agenda_feature.data.repositories.agendaReposit
 import com.realityexpander.tasky.agenda_feature.common.RepositoryResult
 import com.realityexpander.tasky.agenda_feature.data.common.convertersDTOEntityDomain.toDTO
 import com.realityexpander.tasky.agenda_feature.data.repositories.agendaRepository.remote.IAgendaApi
-import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.eventRepositoryImpls.EventRepositoryImpl
 import com.realityexpander.tasky.agenda_feature.domain.AgendaItem
 import com.realityexpander.tasky.agenda_feature.domain.AgendaSync
 import com.realityexpander.tasky.agenda_feature.domain.IAgendaRepository
 import com.realityexpander.tasky.agenda_feature.domain.IEventRepository
 import com.realityexpander.tasky.agenda_feature.util.EventId
+import kotlinx.coroutines.flow.Flow
 import java.time.ZonedDateTime
+import javax.inject.Inject
 
-class AgendaRepositoryImpl(
-    private val eventRepository: IEventRepository = EventRepositoryImpl(),
+class AgendaRepositoryImpl @Inject constructor(
+    private val eventRepository: IEventRepository, // = EventRepositoryImpl(),
 //    private val taskRepository: ITaskRepository,                                  // todo implement tasks repo
 //    private val reminderRepository: IReminderRepository,                          // todo implement reminders repo
     private val agendaApi: IAgendaApi,
 ) : IAgendaRepository {
 
-    override suspend fun getAgendaDay(dateTime: ZonedDateTime): List<AgendaItem> {
+    override suspend fun getAgendaForDay(dateTime: ZonedDateTime): List<AgendaItem> {
         val events = eventRepository.getEventsForDay(dateTime)
+//        val tasks = taskRepository.getTasks(dateTime)                             // todo implement tasks repo
+//        val reminders = reminderRepository.getReminders(dateTime)                 // todo implement reminders repo
+        return events // + tasks + reminders
+    }
+
+    override fun getAgendaForDayFlow(dateTime: ZonedDateTime): Flow<List<AgendaItem>> {
+        val events = eventRepository.getEventsForDayFlow(dateTime)
 //        val tasks = taskRepository.getTasks(dateTime)                             // todo implement tasks repo
 //        val reminders = reminderRepository.getReminders(dateTime)                 // todo implement reminders repo
         return events // + tasks + reminders
@@ -51,7 +59,7 @@ class AgendaRepositoryImpl(
     }
 
     override suspend fun getEvent(eventId: EventId): AgendaItem.Event? {
-        return eventRepository.getEventId(eventId)
+        return eventRepository.getEvent(eventId)
     }
 
     override suspend fun updateEvent(event: AgendaItem.Event): RepositoryResult {
