ad7fd2d85eb579a09a95844c863b481fa62e096e
Chris Athanas <realityexpander@gmail.com>
2022-11-09 23:26:17 -0600

    Add UTC <-> ZonedDateTime coverters

--- app/src/main/java/com/realityexpander/tasky/core/util/UTCtoZonedDateTimeExtensions.kt
+++ app/src/main/java/com/realityexpander/tasky/core/util/UTCtoZonedDateTimeExtensions.kt
@@ -4,13 +4,11 @@ import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
-typealias UtcLong = Long
-
-fun ZonedDateTime.toUtcLong(): UtcLong {
+fun ZonedDateTime.toUtcMillis(): UtcMillis {
     return this.toOffsetDateTime().toInstant().toEpochMilli()
 }
 
-fun UtcLong.toZonedDateTime(zoneId: String = ZoneId.systemDefault().id): ZonedDateTime {
+fun UtcMillis.toZonedDateTime(zoneId: String = ZoneId.systemDefault().id): ZonedDateTime {
     return ZonedDateTime.ofInstant(
         Instant.ofEpochMilli(this),
         ZoneId.of(zoneId)
