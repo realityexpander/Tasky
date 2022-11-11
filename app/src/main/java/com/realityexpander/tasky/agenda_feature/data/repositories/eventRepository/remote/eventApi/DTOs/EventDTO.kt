facdfdd214c2fb6d806ac64513c051fdac51f3a2
Chris Athanas <realityexpander@gmail.com>
2022-11-11 03:01:24 -0600

    remove old code, user @Required for required fields, @Transient for un-serialized fields

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/eventRepository/remote/eventApi/DTOs/EventDTO.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/eventRepository/remote/eventApi/DTOs/EventDTO.kt
@@ -5,34 +5,7 @@ import com.realityexpander.tasky.agenda_feature.util.*
 import com.realityexpander.tasky.core.util.UserId
 import com.realityexpander.tasky.core.util.UtcMillis
 import com.realityexpander.tasky.core.util.UuidStr
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
-
-//@Keep
-//@Serializable
-//@OptIn(ExperimentalSerializationApi::class)
-//data class EventDTO1(
-//    val id: UuidStr,
-//    val title: String,
-//    val description: String,
-//    val remindAt: UtcMillis,
-//    val from: UtcMillis,
-//    val to: UtcMillis,
-//
-//    val host: UserId? = null,
-//    val isUserEventCreator: Boolean? = null,
-//    val isGoing: Boolean? = null,
-//
-//    @SerialName("attendeeIds") // output to json
-//    @JsonNames("attendees") // input from json
-//    val attendeeIds: List<AttendeeId> = listOf(attendeeId(emptyId)),
-//
-//    val photos: List<PhotoId> = emptyList(),
-//    val deletedPhotoKeys: List<PhotoId> = emptyList(),
-//)
-
+import kotlinx.serialization.*
 
 abstract class EventDTO {
 
@@ -55,10 +28,11 @@ abstract class EventDTO {
         override val from: UtcMillis,
         override val to: UtcMillis,
 
-        val attendeeIds: List<AttendeeId> = listOf(attendeeId(emptyId)),  // must include field in request, even if empty.
+        @Required  // forces write empty list if empty -> [] instead of no-field-written
+        val attendeeIds: List<AttendeeId> = emptyList(),
 
         @Transient
-        val photos: List<PhotoDTO> = emptyList(),
+        val photos: List<PhotoDTO> = emptyList(),  // todo do uploading of photos
     ) : EventDTO()
 
     @Keep
@@ -73,13 +47,16 @@ abstract class EventDTO {
         override val to: UtcMillis,
 
         val isGoing : Boolean = true,
-        val attendeeIds: List<AttendeeId> = listOf(attendeeId(emptyId)), // must include field in request, even if empty
 
+        @Required  // forces write empty list if empty -> [] instead of no-field-written
+        val attendeeIds: List<AttendeeId> = emptyList(),
+
+        @Required
         @SerialName("deletedPhotoKeys") // output to json
-        val deletedPhotoIds: List<PhotoId> = listOf(photoId(emptyId)),  // must include field in request, even if empty.
+        val deletedPhotoIds: List<PhotoId> = emptyList(),
 
         @Transient
-        val photos: List<PhotoDTO> = emptyList(),
+        val photos: List<PhotoDTO> = emptyList(), // todo do uploading of photos
     ) : EventDTO()
 
     @Keep
