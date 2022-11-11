97d641528082cb2e9d632bd6fd9b943a33c961b1
Chris Athanas <realityexpander@gmail.com>
2022-11-11 02:57:09 -0600

    Remove serialization from domain

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/domain/Attendee.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/domain/Attendee.kt
@@ -1,27 +1,23 @@
 package com.realityexpander.tasky.agenda_feature.domain
 
 import android.os.Parcelable
-import com.realityexpander.tasky.agenda_feature.data.common.serializers.ZonedDateTimeSerializer
 import com.realityexpander.tasky.agenda_feature.util.AttendeeId
 import com.realityexpander.tasky.agenda_feature.util.EventId
 import com.realityexpander.tasky.agenda_feature.util.UrlStr
 import com.realityexpander.tasky.core.util.Email
 import kotlinx.parcelize.Parcelize
-import kotlinx.serialization.Serializable
 import java.time.ZonedDateTime
 
 @Parcelize
-@Serializable  // for Room @TypeConverter
 data class Attendee(
     val id: AttendeeId,
-    val eventId: EventId,
+    val eventId: EventId? = null,
 
     val email: Email,
     val fullName: String,
-    val isGoing: Boolean,
+    val isGoing: Boolean = false,
 
-    @Serializable(with = ZonedDateTimeSerializer::class) // for Room @TypeConverter
-    val remindAt: ZonedDateTime,
+    val remindAt: ZonedDateTime? = null,
 
-    val photo: UrlStr,
+    val photo: UrlStr? = null,
 ) : Parcelable
