a48ebbb0a63edf12a72e110bc35c2f08d1335777
Chris Athanas <realityexpander@gmail.com>
2022-11-11 14:47:36 -0600

    Enahance Photo to use separate Local uri and Remote url

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/data/common/convertersDTOEntityDomain/PhotoConverters.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/data/common/convertersDTOEntityDomain/PhotoConverters.kt
@@ -1,30 +1,38 @@
 package com.realityexpander.tasky.agenda_feature.data.common.convertersDTOEntityDomain
 
-import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.remote.eventApi.DTOs.PhotoDTO
 import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.local.entities.PhotoEntity
+import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.remote.eventApi.DTOs.PhotoDTO
 import com.realityexpander.tasky.agenda_feature.domain.Photo
 
 
 fun PhotoEntity.toDomain() =
-    Photo(
+    Photo.Remote(
         id = id,
         url = url,
     )
 
 fun PhotoDTO.toDomain() =
-    Photo(
+    Photo.Remote(
         id = id,
         url = url,
     )
 
-fun Photo.toEntity() =
+fun Photo.Remote.toEntity() =
     PhotoEntity(
         id = id,
         url = url,
     )
 
-fun Photo.toDTO() =
+fun Photo.Remote.toDTO() =
     PhotoDTO(
         id = id,
         url = url,
+    )
+
+// Note: This is used for local photos that are about to uploaded to the server.
+fun Photo.Local.toDTO() =
+    PhotoDTO(
+        id = id,
+        url = "URL_NOT_SET_YET",
+        uri = uri,
     )
--- app/src/main/java/com/realityexpander/tasky/agenda_feature/domain/Photo.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/domain/Photo.kt
@@ -1,14 +1,23 @@
 package com.realityexpander.tasky.agenda_feature.domain
 
+import android.net.Uri
 import android.os.Parcelable
 import com.realityexpander.tasky.agenda_feature.util.PhotoId
 import com.realityexpander.tasky.agenda_feature.util.UrlStr
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Photo(
-    val id: PhotoId,
-    val url: UrlStr,          // url of the photo on server
+sealed class Photo(
+    open val id: PhotoId,
+) : Parcelable {
 
-    val uri: String? = null,  // points to a local file on device (if photo is new)
-) : Parcelable
+    data class Remote(
+        override val id: PhotoId,
+        val url: UrlStr,      // url of the photo on server
+    ) : Photo(id = id)
+
+    data class Local(
+        override val id: PhotoId,
+        val uri: Uri,         // path to the photo on device
+    ) : Photo(id = id)
+}
