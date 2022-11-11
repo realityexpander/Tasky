4f679acac0c05b4572ff1dfb0654cac4a95c840f
Chris Athanas <realityexpander@gmail.com>
2022-11-10 18:32:19 -0600

    Add userIdStr and example string data

--- app/src/main/java/com/realityexpander/tasky/core/util/AuthUtil.kt
+++ app/src/main/java/com/realityexpander/tasky/core/util/AuthUtil.kt
@@ -1,11 +1,12 @@
 package com.realityexpander.tasky.core.util
 
-typealias Username = String
-typealias Email = String
-typealias Password = String
-typealias AuthToken = String
-typealias UuidStr = String
-typealias UserId = UuidStr
+typealias Username = String     // ex: "realityexpander"
+typealias Email = String        // ex: "chris@demo.com"
+typealias Password = String     // ex: "Password1"
+typealias AuthToken = String    // ex: "Bearer eyJ0eXAiOiJKV1QiLCJ...VNy_S1rAX__zzQN27dTmfiALe7PHVC5I"
+typealias UuidStr = String      // ex: "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"
+typealias UserIdStr = String    // ex: "635dc7880806b27dc8ab81ae"
+typealias UserId = UserIdStr
 
 fun authToken(authToken: String?): AuthToken? {
     return authToken
