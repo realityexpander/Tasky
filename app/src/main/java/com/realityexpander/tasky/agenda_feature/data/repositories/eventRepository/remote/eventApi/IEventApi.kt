65caabd043149c3351cac9164bfabbbe26a54b25
Chris Athanas <realityexpander@gmail.com>
2022-11-11 00:46:52 -0600

    Use more specific Event DTO's

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/eventRepository/remote/eventApi/IEventApi.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/eventRepository/remote/eventApi/IEventApi.kt
@@ -5,11 +5,11 @@ import com.realityexpander.tasky.agenda_feature.util.EventId
 
 interface IEventApi {
 
-    suspend fun createEvent(event: EventDTO): EventDTO
+    suspend fun createEvent(event: EventDTO.Create): EventDTO.Response
 
-    suspend fun getEvent(eventId: EventId): EventDTO
+    suspend fun getEvent(eventId: EventId): EventDTO.Response
 
     suspend fun deleteEvent(eventId: EventId): Boolean
 
-    suspend fun updateEvent(event: EventDTO): EventDTO
+    suspend fun updateEvent(event: EventDTO.Update): EventDTO.Response
 }
