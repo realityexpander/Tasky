a876325b897eb936889aaf9b17d96953f3e6dd64
Chris Athanas <realityexpander@gmail.com>
2022-11-11 00:50:20 -0600

    Add tasky database

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/TaskyDatabase.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/TaskyDatabase.kt
@@ -3,9 +3,10 @@ package com.realityexpander.tasky.agenda_feature.data.repositories
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.realityexpander.tasky.agenda_feature.data.common.typeConverters.LocalDateTimeConverter
-import com.realityexpander.tasky.agenda_feature.data.common.typeConverters.UuidStrListConverter
-import com.realityexpander.tasky.agenda_feature.data.common.typeConverters.ZonedDateTimeConverter
+import com.realityexpander.tasky.agenda_feature.data.common.typeConverters.AttendeeListTypeConverter
+import com.realityexpander.tasky.agenda_feature.data.common.typeConverters.PhotoListTypeConverter
+import com.realityexpander.tasky.agenda_feature.data.common.typeConverters.StringListTypeConverter
+import com.realityexpander.tasky.agenda_feature.data.common.typeConverters.ZonedDateTimeTypeConverter
 import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.local.entities.EventEntity
 import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.local.eventDao.eventDaoImpls.EventDaoImpl
 
@@ -20,9 +21,10 @@ import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepositor
     version = 1,
 )
 @TypeConverters(
-    LocalDateTimeConverter::class,
-    ZonedDateTimeConverter::class,
-    UuidStrListConverter::class,
+    ZonedDateTimeTypeConverter::class,
+    StringListTypeConverter::class,     // for List<T> of UserId, PhotoId, UuidStr, etc.
+    AttendeeListTypeConverter::class,   // for List<Attendee>
+    PhotoListTypeConverter::class,      // for List<Photo>
 )
 abstract class TaskyDatabase : RoomDatabase() {
 
