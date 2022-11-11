889ff38ae994329a5d5c42de26595045e1daf0b6
Chris Athanas <realityexpander@gmail.com>
2022-11-11 02:58:39 -0600

    Make serializable for storage in DB

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/eventRepository/local/entities/AttendeeEntity.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/eventRepository/local/entities/AttendeeEntity.kt
@@ -1,17 +1,21 @@
 package com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.local.entities
 
+import com.realityexpander.tasky.agenda_feature.data.common.serializers.ZonedDateTimeSerializer
 import com.realityexpander.tasky.agenda_feature.util.AttendeeId
 import com.realityexpander.tasky.agenda_feature.util.EventId
 import com.realityexpander.tasky.agenda_feature.util.UrlStr
 import com.realityexpander.tasky.core.util.Email
+import kotlinx.serialization.Serializable
 import java.time.ZonedDateTime
 
+@Serializable  // for ZonedDateTimeSerializer
 class AttendeeEntity(
     val id: AttendeeId,
-    val eventId: EventId,
+    val eventId: EventId? = null,
     val email: Email,
     val fullName: String,
     val isGoing: Boolean,
-    val remindAt: ZonedDateTime,
-    val photo: UrlStr
+    @Serializable(with = ZonedDateTimeSerializer::class) // for Room @TypeConverter
+    val remindAt: ZonedDateTime? = null,
+    val photo: UrlStr? = null,
 )
