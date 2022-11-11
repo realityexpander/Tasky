3f952db56bcb6b0fda8d2b7e6dd59d586daf50d1
Chris Athanas <realityexpander@gmail.com>
2022-11-11 00:57:12 -0600

    Remove data from token string

--- app/src/main/java/com/realityexpander/tasky/core/util/AuthUtil.kt
+++ app/src/main/java/com/realityexpander/tasky/core/util/AuthUtil.kt
@@ -3,7 +3,7 @@ package com.realityexpander.tasky.core.util
 typealias Username = String     // ex: "realityexpander"
 typealias Email = String        // ex: "chris@demo.com"
 typealias Password = String     // ex: "Password1"
-typealias AuthToken = String    // ex: "Bearer eyJ0eXAiOiJKV...ALe7PHVC5I" (JWT Token)
+typealias AuthToken = String    // ex: "Bearer XXXXXXXXXXX...XXXXXXXXXX" (JWT Token)
 typealias UuidStr = String      // ex: "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"
 typealias UserIdStr = String    // ex: "635dc7880806b27dc8ab81ae"
 typealias UserId = UserIdStr
