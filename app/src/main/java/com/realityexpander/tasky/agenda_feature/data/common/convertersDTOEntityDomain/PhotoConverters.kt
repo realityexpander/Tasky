415aaf575104c53350d3093994d780a882262051
Chris Athanas <realityexpander@gmail.com>
2022-11-11 21:44:13 -0600

    Clarify the kind of photo being stored in entity

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/data/common/convertersDTOEntityDomain/PhotoConverters.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/data/common/convertersDTOEntityDomain/PhotoConverters.kt
@@ -1,34 +1,38 @@
 package com.realityexpander.tasky.agenda_feature.data.common.convertersDTOEntityDomain
 
-import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.local.entities.PhotoEntity
+import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.local.entities.PhotoRemoteEntity
 import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.remote.eventApi.DTOs.PhotoDTO
 import com.realityexpander.tasky.agenda_feature.domain.Photo
 
-
-fun PhotoEntity.toDomain() =
+// From Entity to Domain
+fun PhotoRemoteEntity.toDomain() =
     Photo.Remote(
         id = id,
         url = url,
     )
 
+// From DTO to Domain
 fun PhotoDTO.Remote.toDomain() =
     Photo.Remote(
         id = id,
         url = url,
     )
 
+// From Domain to Entity
 fun Photo.Remote.toEntity() =
-    PhotoEntity(
+    PhotoRemoteEntity(
         id = id,
         url = url,
     )
 
+// From Domain to DTO
 fun Photo.Remote.toDTO() =
     PhotoDTO.Remote(
         id = id,
         url = url,
     )
 
+// From Domain to DTO
 // Note: This is used for local photos that are about to uploaded to the server. // todo implement photo uploading
 fun Photo.Local.toDTO() =
     PhotoDTO.Local(
