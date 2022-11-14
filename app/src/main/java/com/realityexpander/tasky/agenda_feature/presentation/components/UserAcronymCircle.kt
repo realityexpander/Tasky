17bcd3594b84a483dee14f1062cedb1a2c581124
Chris Athanas <realityexpander@gmail.com>
2022-11-13 19:55:36 -0600

    Fix Acronym text centering and sizing

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/presentation/components/UserAcronymCircle.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/presentation/components/UserAcronymCircle.kt
@@ -1,7 +1,9 @@
 package com.realityexpander.tasky.agenda_feature.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -10,11 +12,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.realityexpander.tasky.agenda_feature.presentation.common.util.getUserAcronym
 import com.realityexpander.tasky.core.presentation.common.modifiers.DP
 import com.realityexpander.tasky.core.presentation.theme.TaskyLightBlue
@@ -27,23 +29,22 @@ fun UserAcronymCircle(
     style: TextStyle = MaterialTheme.typography.h5,
     fontWeight: FontWeight = FontWeight.Bold,
     softWrap: Boolean = false,
-    textAlign: TextAlign = TextAlign.End,
+    textAlign: TextAlign = TextAlign.Center,
     circleBackgroundColor: Color = MaterialTheme.colors.surface,
 ) {
     val acronym = remember(username) {
         getUserAcronym(username)
     }
-    val circleBackColorNative = remember(circleBackgroundColor) {
-        Color(circleBackgroundColor.toArgb())
-    }
 
     if(acronym.isNotEmpty()) {
         Text(
+            text = acronym,
             modifier = modifier
                 .padding(DP.tiny) // padding around the circle
                 .clip(CircleShape).background(circleBackgroundColor)
+                .width(40.dp) // size of the circle
+                .aspectRatio(1f) // make the circle a square
                 .padding(DP.extraSmall), // size of the circle
-            text = acronym,
             style = style,
             fontWeight = fontWeight,
             softWrap = softWrap,
