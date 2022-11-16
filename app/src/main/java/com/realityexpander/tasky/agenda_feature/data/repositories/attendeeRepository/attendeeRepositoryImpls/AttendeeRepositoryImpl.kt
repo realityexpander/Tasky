65275c4054afb0c4e30c629bb5db9c6154018603
Chris Athanas <realityexpander@gmail.com>
2022-11-15 22:35:05 -0600

    Add email validation

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/attendeeRepository/attendeeRepositoryImpls/AttendeeRepositoryImpl.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/data/repositories/attendeeRepository/attendeeRepositoryImpls/AttendeeRepositoryImpl.kt
@@ -4,18 +4,21 @@ import com.realityexpander.tasky.agenda_feature.common.util.EventId
 import com.realityexpander.tasky.agenda_feature.data.common.convertersDTOEntityDomain.toDomain
 import com.realityexpander.tasky.agenda_feature.data.repositories.attendeeRepository.IAttendeeRepository
 import com.realityexpander.tasky.agenda_feature.data.repositories.attendeeRepository.remote.IAttendeeApi
-import com.realityexpander.tasky.agenda_feature.data.repositories.eventRepository.remote.eventApi.DTOs.AttendeeExistsResponseDTO
 import com.realityexpander.tasky.agenda_feature.domain.Attendee
-import com.realityexpander.tasky.core.data.remote.TaskyApi
-import com.realityexpander.tasky.core.data.remote.utils.getErrorBodyMessage
+import com.realityexpander.tasky.auth_feature.domain.validation.ValidateEmail
 import com.realityexpander.tasky.core.util.Email
 import javax.inject.Inject
 
 class AttendeeRepositoryImpl @Inject constructor(
     private val attendeeApi: IAttendeeApi,
+    private val validateEmail: ValidateEmail
 ) : IAttendeeRepository {
 
     override suspend fun getAttendee(email: Email): Result<Attendee> {
+        if(!validateEmail.validate(email)) {
+            return Result.failure(Exception("Invalid email address."))
+        }
+
         val result = attendeeApi.getAttendee(email)
 
         return when (result.isSuccess) {
@@ -26,7 +29,7 @@ class AttendeeRepositoryImpl @Inject constructor(
             false -> Result.failure(
                 Exception(
                     result.exceptionOrNull()?.message
-                        ?: "Api Error - Attendee value for failure not found"
+                        ?: "Api Error - Error message not found"
                 )
             )
         }
@@ -35,40 +38,5 @@ class AttendeeRepositoryImpl @Inject constructor(
     override suspend fun deleteAttendeeFromEvent(eventId: EventId): Boolean {
 //            val deletedAttendeeIds = eventRepository.getDeletedAttendeeIds()  // todo implement soon
         return true
-    }
-}
-
-// Todo move to own file soon
-class AttendeeApiImpl @Inject constructor(
-    private val taskyApi: TaskyApi
-) : IAttendeeApi {
-
-    override suspend fun getAttendee(email: Email): Result<AttendeeExistsResponseDTO> {
-        try {
-            val response = taskyApi.getAttendee(email)
-
-            if (response.isSuccessful) {
-                response.body()?.let { attendeeExistsResponseDTO ->
-                    if (attendeeExistsResponseDTO.doesUserExist) {
-                        return Result.success(attendeeExistsResponseDTO)
-                    } else {
-                        return Result.failure(Exception("User email does not exist."))
-                    }
-                }
-            }
-
-            return Result.failure(Exception(getErrorBodyMessage(response.errorBody()?.string())))
-        } catch (e: Exception) {
-            return Result.failure(e)
-        }
-    }
-
-    override suspend fun deleteAttendeeFromEvent(eventId: EventId): Boolean {
-        return try {
-            taskyApi.deleteAttendee(eventId)
-            true
-        } catch (e: Exception) {
-            false
-        }
     }
 }
