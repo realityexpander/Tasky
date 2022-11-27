fef1b1cf18ecd64112ec0980b4b9483bfe4071a9
Chris Athanas <realityexpander@gmail.com>
2022-11-27 03:10:36 -0600

    Add local repo access more clear

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/domain/IEventRepository.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/domain/IEventRepository.kt
@@ -1,25 +1,26 @@
 package com.realityexpander.tasky.agenda_feature.domain
 
 import com.realityexpander.tasky.agenda_feature.common.util.EventId
+import com.realityexpander.tasky.core.util.UserId
 import kotlinx.coroutines.flow.Flow
 import java.time.ZonedDateTime
 
 interface IEventRepository {
     suspend fun createEvent(event: AgendaItem.Event): ResultUiText<AgendaItem.Event>
-    suspend fun upsertEventLocallyOnly(event: AgendaItem.Event): ResultUiText<Void>
+    suspend fun upsertEventLocally(event: AgendaItem.Event): ResultUiText<Void>
 
     suspend fun getEventsForDay(zonedDateTime: ZonedDateTime): List<AgendaItem.Event>
     fun getEventsForDayFlow(zonedDateTime: ZonedDateTime): Flow<List<AgendaItem.Event>>
     suspend fun getEvent(eventId: EventId): AgendaItem.Event?
 
-    suspend fun updateEvent(event: AgendaItem.Event): ResultUiText<AgendaItem.Event>
+    suspend fun updateEvent(event: AgendaItem.Event, loggedInUserId: UserId): ResultUiText<AgendaItem.Event>
 
     // only marks the event as deleted
-    suspend fun deleteEventId(eventId: EventId): ResultUiText<Void>
+    suspend fun deleteEvent(eventId: EventId): ResultUiText<Void>
 
     // gets only the "marked as deleted" events
-    suspend fun getDeletedEventIds(): List<EventId>
-    suspend fun deleteFinallyEventIds(eventIds: List<EventId>): ResultUiText<Void>
+    suspend fun getDeletedEventIdsLocally(): List<EventId>
+    suspend fun deleteEventsFinallyLocally(eventIds: List<EventId>): ResultUiText<Void>
 
     suspend fun clearAllEventsLocally(): ResultUiText<Void>
     suspend fun clearEventsForDayLocally(zonedDateTime: ZonedDateTime): ResultUiText<Void>
