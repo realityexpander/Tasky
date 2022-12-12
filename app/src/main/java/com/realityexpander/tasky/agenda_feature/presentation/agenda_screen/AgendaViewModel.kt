c515bf8bdc6e995abddee90d11adc378aa54fd24
Chris Athanas <realityexpander@gmail.com>
2022-12-12 14:17:48 -0600

    Fix spacing

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/presentation/agenda_screen/AgendaViewModel.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/presentation/agenda_screen/AgendaViewModel.kt
@@ -70,7 +70,6 @@ class AgendaViewModel @Inject constructor(
                 errorMessage = errorMessage,
                 selectedDayIndex = selectedDayIndex,
             ))
-
     val agendaState = combine(
         _agendaState,
         _currentDate,
