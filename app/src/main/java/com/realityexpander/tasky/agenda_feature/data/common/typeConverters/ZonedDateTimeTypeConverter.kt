856e2b4c2a0d51ab21dd919e46433c89cef1866d
Chris Athanas <realityexpander@gmail.com>
2022-12-14 01:02:10 -0600

    improve comments

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/data/common/typeConverters/ZonedDateTimeTypeConverter.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/data/common/typeConverters/ZonedDateTimeTypeConverter.kt
@@ -2,37 +2,11 @@ package com.realityexpander.tasky.agenda_feature.data.common.typeConverters
 
 import androidx.room.TypeConverter
 import com.realityexpander.tasky.core.util.EpochSecond
-import com.realityexpander.tasky.core.util.ZonedDateTimeStr
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
-
-class ZonedDateTimeTypeConverter2 {
-    @TypeConverter
-    fun fromZonedDateTime(value: ZonedDateTime?): ZonedDateTimeStr? {  // saves as a String (ZonedDateTime Format) in DB Tables
-        return value?.toInstant()
-            ?.atZone(ZoneOffset.UTC)
-            ?.truncatedTo(ChronoUnit.MINUTES)
-            ?.toString()
-    }
-
-    @TypeConverter
-    fun toZonedDateTime(value: ZonedDateTimeStr?): ZonedDateTime? {   // restores from a String (ZonedDateTime Format) in DB Tables
-        return value?.let {
-            ZonedDateTime.ofInstant(
-                Instant.from(
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm'Z'")
-                        .withZone(ZoneId.of("UTC"))
-                        .parse(value)
-                ),
-                ZoneId.systemDefault()  // convert to current local time zone
-            )
-        }
-    }
-}
 
 class ZonedDateTimeTypeConverter {
     @TypeConverter
@@ -42,7 +16,6 @@ class ZonedDateTimeTypeConverter {
             ?.truncatedTo(ChronoUnit.MINUTES)
             ?.toEpochSecond()
     }
-
     @TypeConverter
     fun toZonedDateTime(value: EpochSecond?): ZonedDateTime? {   // restores from a Long (UTC epochSeconds) in DB Tables
         return value?.let {
@@ -52,9 +25,8 @@ class ZonedDateTimeTypeConverter {
             )
         }
     }
+
 }
-
-
 
 //// Local Test ////
 
@@ -86,3 +58,44 @@ fun main() {
     println(nycNowLong2)
     println(nycNowLong == nycNowLong2)
 }
+
+
+// LEFT FOR REFERENCE
+// Room database does query comparisons in the SQL database based on primitives only. So, if you
+// use a custom type, you need to convert it to a primitive type that Room will use to make
+// comparisons in the SQL queries. For dates this is always going to be a long.
+//
+// For example, if you have a custom type called Date, you should convert it to a long value that
+// represents the number of milliseconds since January 1, 1970, 00:00:00 GMT. Then, Room will use
+// the long value to compare dates in queries. If you don't convert the custom type to a primitive
+// type that Room can compare (like a long), Room will compare the string representation of the
+// custom type, which is not what you want.
+//
+// https://developer.android.com/training/data-storage/room/referencing-data
+//
+// This shows the wrong approach to converting a custom type that needs to be compared using a Room query.
+// In this incorrect example, the custom type is a Date object. The Date object is converted to a string.
+// Room uses the string representation of the Date object to compare dates. This is not what you want.
+//class ZonedDateTimeTypeConverter2 {
+//    @TypeConverter
+//    fun fromZonedDateTime(value: ZonedDateTime?): ZonedDateTimeStr? {  // saves as a String (ZonedDateTime Format) in DB Tables
+//        return value?.toInstant()
+//            ?.atZone(ZoneOffset.UTC)
+//            ?.truncatedTo(ChronoUnit.MINUTES)
+//            ?.toString()
+//    }
+//
+//    @TypeConverter
+//    fun toZonedDateTime(value: ZonedDateTimeStr?): ZonedDateTime? {   // restores from a String (ZonedDateTime Format) in DB Tables
+//        return value?.let {
+//            ZonedDateTime.ofInstant(
+//                Instant.from(
+//                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm'Z'")
+//                        .withZone(ZoneId.of("UTC"))
+//                        .parse(value)
+//                ),
+//                ZoneId.systemDefault()  // convert to current local time zone
+//            )
+//        }
+//    }
+//}
