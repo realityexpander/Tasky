13c263b7ab5c2956f9e82489504354fb573652c6
Chris Athanas <realityexpander@gmail.com>
2022-12-22 22:02:50 -0600

    refactor to make single source of transition settings

--- app/src/main/java/com/realityexpander/tasky/core/presentation/animatedTransitions/ScreenTransitions.kt
+++ app/src/main/java/com/realityexpander/tasky/core/presentation/animatedTransitions/ScreenTransitions.kt
@@ -10,63 +10,53 @@ import com.realityexpander.tasky.destinations.AgendaScreenDestination
 @OptIn(ExperimentalAnimationApi::class)
 object ScreenTransitions : DestinationStyle.Animated {
 
+    val slideInHorizontally = slideInHorizontally(
+        initialOffsetX = { 1000 },
+        animationSpec = tween(500)
+    )
+    val slideInHorizontallyReverse = slideInHorizontally(
+        initialOffsetX = { -1000 },
+        animationSpec = tween(500)
+    )
+
+    val slideOutHorizontally = slideOutHorizontally(
+        targetOffsetX = { 1000 },
+        animationSpec = tween(500)
+    )
+    val slideOutHorizontallyReverse = slideOutHorizontally(
+        targetOffsetX = { -1000 },
+        animationSpec = tween(500)
+    )
+
     override fun AnimatedContentScope<NavBackStackEntry>.enterTransition(): EnterTransition? {
 
         return when (initialState.appDestination()) {
-            AgendaScreenDestination ->
-                    slideInHorizontally(
-                    initialOffsetX = { 1000 },
-                    animationSpec = tween(700)
-                )
-            else -> slideInHorizontally(
-                    initialOffsetX = { -1000 },
-                    animationSpec = tween(700)
-                )
+            AgendaScreenDestination -> slideInHorizontally
+            else -> slideInHorizontallyReverse
         }
     }
 
     override fun AnimatedContentScope<NavBackStackEntry>.exitTransition(): ExitTransition? {
 
         return when (targetState.appDestination()) {
-            AgendaScreenDestination ->
-                    slideOutHorizontally(
-                    targetOffsetX = { 1000 },
-                    animationSpec = tween(700)
-                )
-            else -> slideOutHorizontally(
-                    targetOffsetX = { -1000 },
-                    animationSpec = tween(700)
-                )
+            AgendaScreenDestination -> slideOutHorizontally
+            else -> slideOutHorizontallyReverse
         }
     }
 
     override fun AnimatedContentScope<NavBackStackEntry>.popEnterTransition(): EnterTransition? {
 
         return when (initialState.appDestination()) {
-            AgendaScreenDestination ->
-                    slideInHorizontally(
-                    initialOffsetX = { 1000 },
-                    animationSpec = tween(700)
-                )
-            else -> slideInHorizontally(
-                    initialOffsetX = { -1000 },
-                    animationSpec = tween(700)
-                )
+            AgendaScreenDestination -> slideInHorizontally
+            else -> slideInHorizontallyReverse
         }
     }
 
     override fun AnimatedContentScope<NavBackStackEntry>.popExitTransition(): ExitTransition? {
 
         return when (targetState.appDestination()) {
-            AgendaScreenDestination ->
-                    slideOutHorizontally(
-                    targetOffsetX = { 1000 },
-                    animationSpec = tween(700)
-                )
-            else -> slideOutHorizontally(
-                    targetOffsetX = { -1000 },
-                    animationSpec = tween(700)
-                )
+            AgendaScreenDestination -> slideOutHorizontally
+            else -> slideOutHorizontallyReverse
         }
     }
 }
