815abd02c0114fa51a93f7e21199a70cfa468aba
Chris Athanas <realityexpander@gmail.com>
2022-12-27 18:27:13 -0600

    remove old code

--- app/src/main/java/com/realityexpander/tasky/core/data/settings/AppSettings.kt
+++ app/src/main/java/com/realityexpander/tasky/core/data/settings/AppSettings.kt
@@ -8,32 +8,3 @@ data class AppSettings(
     val authInfo: AuthInfo? = null,
     var isSettingsInitialized: Boolean = false  // allows us to check if the settings is loaded
 )
-
-//suspend fun DataStore<AppSettings>.saveAuthInfo(authInfo: AuthInfo) {
-//    updateData { appSettings ->
-//        appSettings.copy(authInfo = authInfo)
-//    }
-//}
-//
-//suspend fun DataStore<AppSettings>.saveSettingsInitialized(firstTime: Boolean) {
-//    updateData { appSettings ->
-//        appSettings.copy(isSettingsInitialized = firstTime)
-//    }
-//}
-
-//class AppSettingsRepositoryImpl @Inject constructor(
-//    override val dataStore: DataStore<AppSettings>
-//) : IAppSettingsRepository {
-//
-//    override suspend fun saveAuthInfo(authInfo: AuthInfo) {
-//        dataStore.updateData { appSettings ->
-//            appSettings.copy(authInfo = authInfo)
-//        }
-//    }
-//
-//    override suspend fun saveSettingsIsInitialized(firstTime: Boolean) {
-//        dataStore.updateData { appSettings ->
-//            appSettings.copy(isSettingsInitialized = firstTime)
-//        }
-//    }
-//}
