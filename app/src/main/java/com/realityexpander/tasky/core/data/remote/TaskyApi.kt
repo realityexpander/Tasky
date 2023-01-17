fbe097da53d86d88f0531db2c4181f1d624c5bb7
Chris Athanas <realityexpander@gmail.com>
2023-01-17 17:57:01 -0600

    Update to show api example properties file

--- app/src/main/java/com/realityexpander/tasky/core/data/remote/TaskyApi.kt
+++ app/src/main/java/com/realityexpander/tasky/core/data/remote/TaskyApi.kt
@@ -24,8 +24,8 @@ import retrofit2.http.*
 interface TaskyApi {
 
     companion object {
-//        const val BASE_URL = "https://tasky.pl-coding.com/"
-        const val BASE_URL = "http://localhost:8080/"
+        const val BASE_URL = "https://tasky.pl-coding.com/"
+//        const val BASE_URL = "http://localhost:8080/" // for local testing of the backend
         const val API_KEY = BuildConfig.API_KEY
     }
 
