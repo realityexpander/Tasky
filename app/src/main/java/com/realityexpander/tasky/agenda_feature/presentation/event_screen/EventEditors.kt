2a23593d564cd1768756e0dc246ecef46e8be392
Chris Athanas <realityexpander@gmail.com>
2025-05-22 14:58:17 -0500

    add Compose Previews for EventPropertyEditors

--- app/src/main/java/com/realityexpander/tasky/agenda_feature/presentation/event_screen/EventEditors.kt
+++ app/src/main/java/com/realityexpander/tasky/agenda_feature/presentation/event_screen/EventEditors.kt
@@ -1,35 +1,58 @@
 package com.realityexpander.tasky.agenda_feature.presentation.event_screen
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Error
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import com.realityexpander.tasky.R
+import com.realityexpander.tasky.agenda_feature.domain.Photo
 import com.realityexpander.tasky.agenda_feature.presentation.common.components.EditTextModal
 import com.realityexpander.tasky.agenda_feature.presentation.common.util.toZonedDateTime
-import com.realityexpander.tasky.agenda_feature.presentation.event_screen.EventScreenEvent.*
+import com.realityexpander.tasky.agenda_feature.presentation.event_screen.EventScreenEvent.CancelEditMode
+import com.realityexpander.tasky.agenda_feature.presentation.event_screen.EventScreenEvent.ClearErrorMessage
+import com.realityexpander.tasky.agenda_feature.presentation.event_screen.EventScreenEvent.ClearErrorsForAddAttendeeDialog
+import com.realityexpander.tasky.agenda_feature.presentation.event_screen.EventScreenEvent.EditMode
+import com.realityexpander.tasky.agenda_feature.presentation.event_screen.EventScreenEvent.OneTimeEvent
+import com.realityexpander.tasky.agenda_feature.presentation.event_screen.EventScreenEvent.SetIsEditable
+import com.realityexpander.tasky.agenda_feature.presentation.event_screen.EventScreenEvent.ValidateAttendeeEmail
+import com.realityexpander.tasky.agenda_feature.presentation.event_screen.EventScreenEvent.ValidateAttendeeEmailExistsThenAddAttendee
 import com.realityexpander.tasky.agenda_feature.presentation.event_screen.components.PhotoModal
 import com.realityexpander.tasky.core.presentation.common.modifiers.DP
 import com.realityexpander.tasky.core.presentation.common.modifiers.mediumHeight
 import com.realityexpander.tasky.core.presentation.common.modifiers.smallHeight
 import com.realityexpander.tasky.core.presentation.common.modifiers.tinyHeight
+import com.realityexpander.tasky.core.presentation.theme.TaskyTheme
+import com.realityexpander.tasky.core.presentation.util.UiText
 import com.vanpra.composematerialdialogs.MaterialDialog
 import com.vanpra.composematerialdialogs.datetime.date.datepicker
 import com.vanpra.composematerialdialogs.datetime.time.timepicker
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import java.time.LocalDateTime
+import java.time.ZonedDateTime
 
 @Composable
 fun EventPropertyEditors(
@@ -55,6 +78,7 @@ fun EventPropertyEditors(
                 onAction(CancelEditMode)
             }
         }
+
         is EditMode.ChooseFromDate,
         is EditMode.ChooseToDate -> {
             editMode as EditMode.DateTimePayload
@@ -82,6 +106,7 @@ fun EventPropertyEditors(
                 }
             }
         }
+
         is EditMode.ChooseFromTime,
         is EditMode.ChooseToTime -> {
             editMode as EditMode.DateTimePayload
@@ -109,12 +134,15 @@ fun EventPropertyEditors(
                 }
             }
         }
+
         is EditMode.ChooseRemindAtDateTime -> { // handled in the RemindAt UI element, this is here to remove compiler warning
         }
+
         is EditMode.ChooseAddPhoto -> {
             onAction(CancelEditMode)
             onAction(OneTimeEvent.LaunchPhotoPicker)
         }
+
         is EditMode.ViewOrRemovePhoto -> {
             PhotoModal(
                 photo = editMode.photo,
@@ -128,6 +156,7 @@ fun EventPropertyEditors(
                 }
             )
         }
+
         is EditMode.ChooseAddAttendee -> {
             val addAttendeeDialogState = rememberMaterialDialogState()
             var attendeeEmail by remember { mutableStateOf("") }
@@ -241,6 +270,7 @@ fun EventPropertyEditors(
                 Spacer(modifier = Modifier.smallHeight())
             }
         }
+
         is EditMode.ConfirmRemoveAttendee -> {
             val attendee = editMode.attendee
             val removeAttendeeDialogState = rememberMaterialDialogState()
@@ -296,6 +326,143 @@ fun EventPropertyEditors(
                 )
                 Spacer(modifier = Modifier.smallHeight())
             }
+        }
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun EventPropertyEditors_ChposeFromDate() {
+    val previewContext = LocalContext.current
+
+    TaskyTheme {
+        CompositionLocalProvider(
+            LocalContext provides previewContext
+        ) {
+            EventPropertyEditors(
+                editMode = EditMode.ChooseFromDate(
+                    dialogTitle = UiText.Str("Choose From Date"),
+                    dateTime = ZonedDateTime.now()
+                ),
+                state = EventScreenState(),
+                onAction = {}
+            )
+        }
+    }
+}
+
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    widthDp = 500,
+    heightDp = 400,
+    backgroundColor = 0xFF000000,
+    showBackground = true
+)
+@Composable
+fun EventPropertyEditors_ViewOrRemovePhoto() {
+    val previewContext = LocalContext.current
+
+    TaskyTheme {
+        CompositionLocalProvider(
+            LocalContext provides previewContext
+        ) {
+            EventPropertyEditors(
+                editMode = EditMode.ViewOrRemovePhoto(
+                    photo = Photo.Remote("12345", "https://randomuser.me/api/portraits/men/75.jpg"),
+                    dialogTitle = UiText.Str("View or Remove Photo"),
+                ),
+                state = EventScreenState(
+                    showAlertDialog = EventScreenEvent.ShowAlertDialog(
+                        title = UiText.Str("Remove Photo"),
+                        message = UiText.Str("Are you sure you want to remove this photo?"),
+                        onConfirm = {},
+                        isDismissButtonVisible = true,
+                        confirmButtonLabel = UiText.Str("Yes"),
+                    ),
+                    isLoaded = false,
+                    isEditable = true,
+                    isProgressVisible = true,
+                    errorMessage = UiText.Str("Error loading photo"),
+                ),
+                onAction = {}
+            )
+        }
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    widthDp = 400,
+    heightDp = 300,
+)
+@Composable
+fun EventPropertyEditors_ChooseDescriptionText() {
+    val previewContext = LocalContext.current
+
+    TaskyTheme {
+        CompositionLocalProvider(
+            LocalContext provides previewContext
+        ) {
+            EventPropertyEditors(
+                editMode = EditMode.ChooseDescriptionText(
+                    "This is the text to edit",
+                    dialogTitle = UiText.Str("Choose Description"),
+                ),
+                state = EventScreenState(),
+                onAction = {}
+            )
+        }
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    widthDp = 400,
+    heightDp = 300,
+)
+@Composable
+fun EventPropertyEditors_ChooseTitleText() {
+    val previewContext = LocalContext.current
+
+    TaskyTheme {
+        CompositionLocalProvider(
+            LocalContext provides previewContext
+        ) {
+            EventPropertyEditors(
+                editMode = EditMode.ChooseTitleText(
+                    "This is the text to edit",
+                    dialogTitle = UiText.Str("Choose Title"),
+                ),
+                state = EventScreenState(),
+                onAction = {}
+            )
+        }
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    widthDp = 400,
+    heightDp = 300,
+)
+@Composable
+fun EventPropertyEditors_ChooseAddAttendee() {
+    val previewContext = LocalContext.current
+
+    TaskyTheme {
+        CompositionLocalProvider(
+            LocalContext provides previewContext
+        ) {
+            EventPropertyEditors(
+                editMode = EditMode.ChooseAddAttendee(
+                    dialogTitle = UiText.Str("Choose Add Attendee"),
+                ),
+                state = EventScreenState(),
+                onAction = {}
+            )
         }
     }
 }
